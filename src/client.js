31f3b7d7dd3605e8caa2006c893ed1997ca4f3c9
xiyuesaves <xiyuesaves@gmail.com>
2025-09-12 12:26:33 +0800

    feat: 新增过滤日志类型

--- src/client.js
+++ src/client.js
@@ -5,6 +5,14 @@ window.superjson = superjson;
 const port = location.port;
 console.log("通信端口", port);
 
+let filter = "All";
+const logMap = new Map([
+  ["All", []],
+  ["send", []],
+  ["receive", []],
+  ["other", []],
+]);
+
 async function poll() {
   try {
     let logs = superjson.parse(await (await fetch(`http://localhost:${port}/`)).text());
@@ -19,14 +27,23 @@ async function poll() {
           case "[send]":
             log[0] = `%c[send]`;
             log.splice(1, 0, "background:#87e8de;color:#000000D9;padding:2px 4px;border-radius: 4px;width:56px;");
+            logMap.get("All").push(log);
+            logMap.get("send").push(log);
+            if (!["send", "All"].includes(filter)) continue;
             break;
           case "[receive]":
             log[0] = `%c[receive]`;
             log.splice(1, 0, "background:#b7eb8f;color:#000000D9;padding:2px 4px;border-radius: 4px;width:56px;");
+            logMap.get("All").push(log);
+            logMap.get("receive").push(log);
+            if (!["receive", "All"].includes(filter)) continue;
             break;
           default:
             log[0] = `%c${log[0]}`;
             log.splice(1, 0, "background:#ffdc00;color:#000000D9;padding:2px 4px;border-radius: 4px;");
+            logMap.get("All").push(log);
+            logMap.get("other").push(log);
+            if (!["other", "All"].includes(filter)) continue;
         }
         console.log(...log);
       }
@@ -44,3 +61,24 @@ async function poll() {
 }
 
 poll();
+
+function initFilter() {
+  // 获取所有同名单选框
+  const radios = document.querySelectorAll('input[name="filter"]');
+  // 遍历添加监听
+  radios.forEach((radio) => {
+    radio.addEventListener("change", (event) => {
+      if (event.target.checked) {
+        console.log("选中了：", event.target.value);
+        filter = event.target.value;
+        console.clear();
+        const filterArr = logMap.get(filter);
+        for (let i = 0; i < filterArr.length; i++) {
+          console.log(...filterArr[i]);
+        }
+      }
+    });
+  });
+}
+
+initFilter();
